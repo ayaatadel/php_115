83db02d1786e57b91640e487ebce725de060ce44
ayaatadel <ayaatadel128@gmail.com>
2024-10-02 20:29:13 +0300

    rename test file to tasks file

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-echo "<h1 style='text-align:center;margin-top:15px'> hello  group 115 back end </h1>";
+echo "<h1 style='text-align:center;margin-top:15px'> hello  group 115 back end  day 1 </h1>";
 
 
 
---
+++ tasks.php
@@ -0,0 +1,17 @@
+<?php 
+
+
+/*
+// install git
+account on git hub
+account on linkedIn
+// serach how to make branches on github and use it
+
+*/
+
+
+
+
+
+
+?>
