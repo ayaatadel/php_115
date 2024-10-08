86b3fdfe6d86dbc6b04014510b462cd7bb3dd309
ayaatadel <ayaatadel128@gmail.com>
2024-10-08 21:04:09 +0300

    erd session

--- tasks.php
+++ tasks.php
@@ -1,11 +1,18 @@
 <?php 
 
+print("tasks file ")
 
 /*
 // install git
 account on git hub
 account on linkedIn
 // serach how to make branches on github and use it
+
+// files of problems that ypu handle it using code
+
+/////////////// your choice 
+
+// install laragon and use it in coding
 
 */
 
